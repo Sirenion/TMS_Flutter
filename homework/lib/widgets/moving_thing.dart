cf518473d35519dc9dcc48099b0c9d66b7ad1f9c
Sirenion <gizmode338@gmail.com>
2026-01-11 14:26:23 +0300

    11-01
    TweenAnimationBuilder

--- homework/lib/widgets/moving_thing.dart
+++ homework/lib/widgets/moving_thing.dart
@@ -8,38 +8,67 @@ class MovingThing extends StatefulWidget {
 }
 
 class _MovingThingState extends State<MovingThing> {
-  bool _left = true;
+  bool _moveForward = true;
 
   @override
   Widget build(BuildContext context) {
     final double width = MediaQuery.of(context).size.width;
+    const double circleSize = 75.0;
     return SizedBox(
       height: 150.0,
       width: width,
-      child: Stack(
-        children: [
-          AnimatedPositioned(
-            left: _left ? 0 : (width - 105.0),
-            top: _left ? 0 : 75.0,
-            duration: const Duration(seconds: 1),
-            child: GestureDetector(
-              onTap: () {
-                setState(() {
-                  _left = !_left;
-                });
-              },
-              child: Container(
-                width: 75.0,
-                height: 75.0,
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                  color: Colors.black, // changes position of shadow
-                ),
-              ),
+      child: TweenAnimationBuilder(
+        tween: Tween<double>(begin: _moveForward ? 0.0 : 1.0, end: _moveForward ? 1.0 : 0.0),
+        duration: const Duration(seconds: 2),
+        curve: Curves.easeOut,
+        builder: (context, value, child) {
+          return Stack(
+            children: [
+              Positioned(left: value * (width - 105.0), top: value * circleSize, child: child!),
+            ],
+          );
+        },
+        child: GestureDetector(
+          onTap: () {
+            setState(() {
+              _moveForward = !_moveForward;
+            });
+          },
+          child: Container(
+            width: circleSize,
+            height: circleSize,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(50.0)),
+              color: Colors.black, // changes position of shadow
             ),
           ),
-        ],
+        ),
       ),
     );
+    // child: Stack(
+    //   children: [
+    //     AnimatedPositioned(
+    //       left: _left ? 0 : (width - 105.0),
+    //       top: _left ? 0 : circleSize,
+    //       duration: const Duration(seconds: 1),
+    //       child: GestureDetector(
+    //         onTap: () {
+    //           setState(() {
+    //             _left = !_left;
+    //           });
+    //         },
+    //         child: Container(
+    //           width: circleSize,
+    //           height: circleSize,
+    //           decoration: const BoxDecoration(
+    //             borderRadius: BorderRadius.all(Radius.circular(50.0)),
+    //             color: Colors.black, // changes position of shadow
+    //           ),
+    //         ),
+    //       ),
+    //     ),
+    //   ],
+    // ),
+    // );
   }
 }
